aed1de0cc525188086b3fc9c8b52818d76b2be4f
Dieter Heidrich <dnbheidrich@gmail.com>
2020-03-19 23:13:41 -0600

    works

--- Program.cs
+++ Program.cs
@@ -22,11 +22,75 @@ namespace rps
                 }
                 Console.Clear();
                 Console.WriteLine("Rock, Paper, or Scissors?");
-                string playerChoice = Console.ReadLine().ToLower().Trim();
                 Random rnd = new Random();
                 int compChoice = rnd.Next(3);
 
-                // ????????
+
+                if (Console.ReadKey().KeyChar == 'r')
+                {
+                     if (compChoice == 1)
+                {
+                Console.WriteLine($"Comp chose rock");
+                Console.WriteLine($"YOU TIE!");
+
+                } else if(compChoice == 2)
+                {
+                Console.WriteLine($"Comp chose paper");
+                Console.WriteLine($"YOU LOSE!");
+                } else 
+                {
+                Console.WriteLine($"Comp chose scissors");
+                Console.WriteLine($"YOU WIN!");
+                }
+                } else if (Console.ReadKey().KeyChar == 'p')
+                {
+                     if (compChoice == 1)
+                {
+                Console.WriteLine($"Comp chose rock");
+                Console.WriteLine($"YOU WIN!");
+
+                } else if(compChoice == 2)
+                {
+                Console.WriteLine($"Comp chose paper");
+                Console.WriteLine($"YOU TIE!");
+                } else 
+                {
+                Console.WriteLine($"Comp chose scissors");
+                Console.WriteLine($"YOU LOSE! ");
+                }
+                } else if (Console.ReadKey().KeyChar == 's')
+                {
+                if (compChoice == 1)
+                {
+                Console.WriteLine($"Comp chose rock");
+                Console.WriteLine($"YOU LOSE!");
+                } else if(compChoice == 2)
+                {
+                Console.WriteLine($"Comp chose paper");
+                Console.WriteLine($"YOU WIN!");
+                } else 
+                {
+                Console.WriteLine($"Comp chose scissors");
+                Console.WriteLine($"YOU TIE!");
+                }
+                }
+
+
+                // string playerChoice = Console.ReadLine().ToLower().Trim();
+                // if (compChoice == 1)
+                // {
+                // Console.WriteLine($"Comp chose rock");
+                // } else if(compChoice == 2)
+                // {
+                // Console.WriteLine($"Comp chose paper");
+
+                // } else 
+                // {
+                // Console.WriteLine($"Comp chose scissors");
+
+                // }
+                // Console.WriteLine($"You chose {playerChoice}");
+
             }
 
         }
