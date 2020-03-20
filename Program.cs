15acd234e0d86475aeac56946c7b80c1e4e210ff
Dieter Heidrich <dnbheidrich@gmail.com>
2020-03-20 09:46:53 -0600

    changes

--- Program.cs
+++ Program.cs
@@ -25,54 +25,74 @@ namespace rps
                 Console.WriteLine("Rock, Paper, or Scissors?");
                 Random rnd = new Random();
                 int compChoice = rnd.Next(3);
-
-
                 if (Console.ReadKey().KeyChar == 'r')
                 {
                      if (compChoice == 1)
                 {
+
                 Console.WriteLine($"Comp chose rock");
                 Console.WriteLine($"YOU TIE!");
 
                 } else if(compChoice == 2)
                 {
+
                 Console.WriteLine($"Comp chose paper");
                 Console.WriteLine($"YOU LOSE!");
+
                 } else 
                 {
+
                 Console.WriteLine($"Comp chose scissors");
                 Console.WriteLine($"YOU WIN!");
+
                 }
                 } else if (Console.ReadKey().KeyChar == 'p')
                 {
+                    
                      if (compChoice == 1)
                 {
+
                 Console.WriteLine($"Comp chose rock");
                 Console.WriteLine($"YOU WIN!");
 
                 } else if(compChoice == 2)
+
                 {
+
                 Console.WriteLine($"Comp chose paper");
                 Console.WriteLine($"YOU TIE!");
+
                 } else 
                 {
+                    
                 Console.WriteLine($"Comp chose scissors");
                 Console.WriteLine($"YOU LOSE! ");
+
                 }
                 } else if (Console.ReadKey().KeyChar == 's')
+
                 {
                 if (compChoice == 1)
+
                 {
+
                 Console.WriteLine($"Comp chose rock");
                 Console.WriteLine($"YOU LOSE!");
+
                 } else if(compChoice == 2)
+
                 {
+
                 Console.WriteLine($"Comp chose paper");
                 Console.WriteLine($"YOU WIN!");
+
                 } else 
+
                 {
+
                 Console.WriteLine($"Comp chose scissors");
                 Console.WriteLine($"YOU TIE!");
+
                 }
                 }
 
